e999f6384ef12df045f36ae8a777c5956eae7563
gedo98 <74098790+gedo98@users.noreply.github.com>
2020-11-10 16:00:54 +0200

    Add files via upload

---
+++ gedo.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int name;
+    int index;
+    int i;
+
+    cout<<"enter your name";
+    cin>>name;
+    cout<<"enter your index";
+cin>>index;
+if (index=='A')
+    cout<<"the student is in control";
+else if(index=='T')
+cout<<"the student is in communication ";
+else if (index == 'c')
+cout<<"the student is in computer";
+int grade;
+if(grade=100&&90)
+cout<<'A';
+else
+if(grade=90&&80)
+cout<<'B';
+else
+if(grade=80&&70)
+cout<<'C';
+else
+if(grade=70&&60)
+cout<<'D';
+else
+if(grade=60&&50)
+cout<<'F';
+else
+cout<<"faied"<<endl;
+cout<<"name"<<endl; 
+cout<<"index number"<<endl;
+cout<<"grade"<<endl;
+cout<<"department"<<endl;
+ return 0;
+ }
+
+
+
+
+
